65d8209e78d3b38057373d422ea09474f59381d7
Steven Capuzzi <stevenczz@gmail.com>
2019-02-09 14:57:44 -0500

    matching similars

--- src/components/Bag.js
+++ src/components/Bag.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react';
 import bag from '../bag.png';
 import burger from '../burger.png';
+import logo from '../logo.png';
+import fries from '../fries.jpg';
 
 
-class Bag extends Component {
+// class Bag extends Component {
 
-//     handleClick(i) {
-//      alert(i)
-//     this.querySelector("burger").classList.add('up');
+//   render() {
+//     return (
+//        <div className='bk-bag'>
+//         <img src={burger} className={this.props.move} />
+//         <img src={bag} className='bag' onClick={this.props.onClick} />
+//        </div>
+//     );
 //   }
-  render() {
+// }
+
+function Bag(props) {
+    let imgg;
+    switch(props.move){
+        case "burger":
+            imgg = burger;
+        break;
+        case "burger-up":
+            imgg = burger;
+        break;
+        case "logo":
+            imgg = logo;
+        break;
+        case "logo-up":
+            imgg = logo;
+        break;
+        case "fries":
+            imgg = fries;
+        break;
+        case "fries-up":
+            imgg = fries;
+        break;
+        
+        }
+            
     return (
        <div className='bk-bag'>
-        <img src={burger} className={this.props.move} />
-        <img src={bag} className='bag' onClick={this.props.onClick} />
+        <img src={imgg} className={props.move} />
+        <img src={bag} className='bag' onClick={props.onClick} />
        </div>
     );
-  }
 }
-
-export default Bag;
+  export default Bag;
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,24 +5,20 @@ import '../App.css';
 class Game extends Component {
     constructor(props) {
         super(props);
-        let arr = ['burger','something','something','something','something','something']
+        let arr = ['burger','logo','burger','logo','fries','fries']
         var currentIndex = arr.length;
         var temporaryValue, randomIndex;
-    
-        // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-            // Pick a remaining element...
             randomIndex = Math.floor(Math.random() * currentIndex);
             currentIndex -= 1;
-    
-            // And swap it with the current element.
             temporaryValue = arr[currentIndex];
             arr[currentIndex] = arr[randomIndex];
             arr[randomIndex] = temporaryValue;
         }
         this.state = {
           bags: arr,
-          match: Array(2).fill(null)
+          match: Array(2).fill(null),
+          score: "Find the Match!"
         };
 
        
@@ -39,20 +35,33 @@ class Game extends Component {
 
     handleClick(i) {
         const bags = this.state.bags.slice();
-        bags[i] = 'up';
+        bags[i] = bags[i]+'-up';
+        let score = null;
         let currentMatch = this.state.match.slice();;
         if(currentMatch[0]!=null){
-            currentMatch[1] = i;
+            currentMatch[1] = bags[i];
         }else{
-            currentMatch[0] = i;
+            currentMatch[0] = bags[i];
         }
         this.setState({bags: bags, match:  currentMatch});
-        if(currentMatch[0]!=null && currentMatch[1]!=null){
-            alert('those are 2');
+        if(currentMatch[1]!=null){
+
+            if(currentMatch[0] === currentMatch[1]){
+                score = "You Win!"
+            }else{
+                score = "You Lose!"
+            }
+            this.setState({score: score});
+
         }
      }
+
   render() {
     return (
+      <div>
+      <div clasName="score">
+        <h1>{this.state.score}</h1>
+      </div>
       <div className="game">
         {this.renderBag(0)}
         {this.renderBag(1)}
@@ -61,6 +70,7 @@ class Game extends Component {
         {this.renderBag(4)}
         {this.renderBag(5)}
       </div>
+      </div>  
     );
   }
 }
